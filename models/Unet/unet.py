8373f36062ab46484f48311944b804c7a2774fbc
Neevkumar Manavar <77490344+neevmanvar@users.noreply.github.com>
2025-03-30 17:08:05 +0200

    Update unet.py

--- models/Unet/unet.py
+++ models/Unet/unet.py
@@ -1,3 +1,7 @@
+# Generator architecture based on U-Net
+# Source: "U-Net: Convolutional Networks for Biomedical Image Segmentation" (Ronneberger et al., 2015)
+# https://arxiv.org/abs/1505.04597
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F

ab873461c9a078db99b9c910c186ebaadc910445
Neevkumar Manavar <77490344+neevmanvar@users.noreply.github.com>
2025-03-30 17:10:55 +0200

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,1 +1,6 @@
+import os
+import sys
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))

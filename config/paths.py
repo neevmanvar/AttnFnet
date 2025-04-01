0ec9d0e869c9ce2104b74aa8cced6b72f649c395
Neevkumar Manavar <77490344+neevmanvar@users.noreply.github.com>
2025-04-01 09:26:12 +0200

    Update paths.py

--- config/paths.py
+++ config/paths.py
@@ -3,19 +3,12 @@ import os
 class dirs:
     ASSET_DIR = "assets"
     TRAINING_PREDICTION_DIR = os.path.join(ASSET_DIR, "training_predictions").replace("\\","/")
-    TRAINING_HISTORY_DIR = os.path.join(ASSET_DIR, "training_history").replace("\\","/")
-    MODEL_SUMMARY_DIR = os.path.join(ASSET_DIR, "model_summary").replace("\\","/")
-    MODEL_PLOTS_DIR = os.path.join(ASSET_DIR, "model_plots").replace("\\","/")
     TEST_PREDICTIONS_DIR = os.path.join(ASSET_DIR, "test_predictions").replace("\\","/")
-    TEST_EVALUATION_DIR = os.path.join(ASSET_DIR, "test_evaluation").replace("\\","/")
     TEST_RESULTS_DIR = os.path.join(ASSET_DIR, "test_results").replace("\\","/")
     TENSORBOARD_RUNS_DIR = "runs"
 
     LOSSES_DIR = "losses"
     METRIC_DIR = "metrics"
-
-    MODEL_CHECKPOINTS_DIR = "model_checkpoints"
-    MODEL_SIGNATURES_DIR = "model_signatures"
 
     DATASET_DIR = "datasets"
     TTV_DATASET_DIR = os.path.join(DATASET_DIR, "ttv").replace("\\","/")

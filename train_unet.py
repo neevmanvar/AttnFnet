c8643d537f920e7dcde368f286de5043c45d3e34
Neevkumar Manavar <77490344+neevmanvar@users.noreply.github.com>
2025-05-07 09:58:51 +0200

    Update train_unet.py

--- train_unet.py
+++ train_unet.py
@@ -5,9 +5,10 @@ from models.Unet.unet import Unet
 from models.discriminator.patchgan import PatchGAN
 from util.prepare_dataset import SLPDataset
 from util.prepare_dataloader import prepare_dataloader
-
+from util.model_callbacks import ModelEncodingToken
 from util.model_trainer import Trainer
 from losses.GANSSIML2Loss import GANSSIML2Loss
+from util.handle_dirs import HandleTrainingDir
 from losses.GANLoss import GANLoss
 import torch
 
@@ -33,9 +34,6 @@ def load_train_objs(cfg: Config, device='cuda'):
     gen_optimizer = optim.Adam(params=gen_model.parameters(), lr=cfg.optimizer.learning_rate, betas=(cfg.optimizer.beta_1, 0.999))
     disc_optimizer = optim.Adam(params=disc_model.parameters(), lr=cfg.optimizer.learning_rate, betas=(cfg.optimizer.beta_1, 0.999))
 
-    # gen_model = torch.compile(gen_model)
-    # disc_model = torch.compile(disc_model)
-
     return train_ds, val_ds, gen_model, disc_model, gen_optimizer, disc_optimizer
 
 @hydra.main(version_base=None, config_name="unet_config")
@@ -51,10 +49,26 @@ def my_hydra_app(cfg: Config)->None:
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"current device: {device}")
+    hd = HandleTrainingDir(cfg)
     train_ds, val_ds, gen_model, disc_model, gen_optimizer, disc_optimizer = load_train_objs(cfg, device = device)
     train_data = prepare_dataloader(dataset=train_ds, is_distributed=False, batch_size=cfg.trainer.batchsize)
     val_data = prepare_dataloader(dataset=val_ds, is_distributed=False, batch_size=1)
-    trainer = Trainer(gen_model, disc_model, gen_optimizer, disc_optimizer, GANSSIML2Loss(), GANLoss(label_smoothing_factors=[0.9, 0.1]), train_data, val_data, device, cfg)
+    
+    token_predictor = ModelEncodingToken(val_data=val_data,
+                                         train_data=train_data,
+                                         use_val_data=True,
+                                         layer_list=[gen_model.d1_act, 
+                                                     gen_model.d3,
+                                                     gen_model.d6],
+                                         block_name_list=["gen_model.d1",
+                                                        "gen_model.d3",
+                                                        "gen_model.d6"],
+                                         batch_index=0,
+                                         save_dir=hd.MODEL_INTERMEDIATE_TOKEN_PREDICION_DIR,
+                                         model_name="unet",
+                                         channel_dim=1)
+    
+    trainer = Trainer(gen_model, disc_model, gen_optimizer, disc_optimizer, GANSSIML2Loss(), GANLoss(label_smoothing_factors=[0.9, 0.1]), train_data, val_data, device, cfg, token_predictor)
     trainer.train(cfg.trainer.max_epochs)
 
 my_hydra_app()
